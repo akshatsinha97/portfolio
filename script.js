6f05594babaae5c45517df2565530d2e2a76ff6d
akshat <akshatsinha97@gmail.com>
2023-01-17 05:42:15 +0530

    changed theme

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-console.log("Script is running...");
 document.querySelector('.cross').style.display = 'none';
-document.querySelector('.hamburger').addEventListener("click",()=>{
-    document.querySelector('.sidebar').classList.toggle('sidebarGo');
-
-if(document.querySelector('.sidebar').classList.contains('sidebarGo')){
-    document.querySelector('.ham').style.display = 'inline';
-    document.querySelector('.cross').style.display = 'none';
-}else{
-    document.querySelector('.ham').style.display = 'none';
+function openNav(){
+    document.querySelector('.sidebar').classList.remove('sidebarGo')
     setTimeout(()=>{    document.querySelector('.cross').style.display = 'inline';    
 }, 350);
-}
-});
+document.querySelector('.ham').style.display = 'none';
+document.querySelector('.main').style.opacity = '0.4';
+document.querySelector('.main').style.zIndex = '-1';
+};
+function closeNav(){
+    document.querySelector('.sidebar').classList.add('sidebarGo')
+    document.querySelector('.cross').style.display = 'none';
+    document.querySelector('.ham').style.display = 'inline';
+    document.querySelector('.main').style.opacity = '';
+    document.querySelector('.main').style.zIndex = '';
+};
